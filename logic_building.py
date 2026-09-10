c9028e7bed714894c5bd50599f5744d4f1cb2916
AtharvDugani <duganiatharv@gmail.com>
2026-09-10 13:58:14 +0530

    version_1.0.0

---
+++ logic_building.py
@@ -0,0 +1,55 @@
+# This is day 2 of Bridge course
+
+# integer = 123
+# float_value = 12.52
+# character = "A"
+# boolean = True
+# string = "Atharv"
+# print(integer)
+# print(float_value)
+# print(character)
+# print(boolean)
+# print(string)
+# multiline_String = """
+# This is 
+# multiline
+# string 
+# """
+# print(type(multiline_String))
+# print(multiline_String)
+# a = int(input("Enter a Number: "))
+# b = 58
+# print("Addition of two number: ",a + b)
+
+# Operator In python
+# Arithmetic Operator: + - * / % // 
+print("Arithmetic Operator\n")
+num1 = 45
+num2 = 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+# Comparision Operator: < > <= >= == != 
+print("Comparision Operator")
+print(num1 < num2)
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 == num2)
+print(num1 != num2)
+
+# logical Operator: and or not
+print("logical Operator")
+print(True and False)
+print(True and True)
+print(True or False)
+print(True or True)
+print(not False)
+name = "Atharv"
+print(f"This is Formated String and I am {name}")
+
+
---
+++ problems.py
@@ -0,0 +1,31 @@
+# problem 1
+# num = int(input("Enter a Number: "))
+# condition = num % 2
+# result = condition == 0 and not condition == 1
+
+# Problem 2
+# age = int(input("Enter your Age: "))
+# print(f"{age} years = {age * 365 } days")
+
+# Problem 3
+# minutes = 190
+# print(f"{minutes} is {minutes // 60} Hours {minutes % 60} Minutes")
+
+# Problem 4
+# num = 1452
+# print(f"{num} : last digit is {num % 10}")
+# num = "12547"
+# print(f"{num} : last digit is {num[-1]}")
+
+# Problem 5
+# role = input("Enter your role (Student / Teacher): ").lower().strip()
+# age = int(input("Enter your age: "))
+# print(f"Eligible : {role == "student" and age < 21}")
+
+# Problem 6
+a = 12
+b = 10
+a = a +b
+b = a - b
+a = a - b
+print (a , b)
